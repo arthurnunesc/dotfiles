aee19f646b2611d39556ab6d6475a3a62ee19694
Arthur Nunes <arthurnunesc@proton.me>
2023-04-24 07:32:51 +0200

    Add some exports to .zshrc and reorder lsp.lua

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,7 @@
+local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 local mason_status, mason = pcall(require, "mason")
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-local status = mason_status and mason_lspconfig_status and lspconfig_status
+local status = lspconfig_status and mason_status and mason_lspconfig_status
 if not status then
   return
 end
