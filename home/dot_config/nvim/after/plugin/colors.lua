3b1763ac66d5afb13f4ee0552eb0c831b7ec65c9
Arthur Nunes <arthurnunesc@proton.me>
2023-05-07 18:05:25 +0200

    Change themes to catppuccin

--- home/dot_config/nvim/after/plugin/colors.lua
+++
@@ -1,8 +0,0 @@
-function SetTheme()
-  vim.cmd[[
-    set termguicolors
-    colorscheme rose-pine
-  ]]
-end
-
-SetTheme()
--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -7,3 +7,5 @@ vim.opt.expandtab = false
 vim.cmd('syntax on')
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- vim.opt.termguicolors = true
